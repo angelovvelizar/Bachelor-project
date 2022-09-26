4e455d45962af61a183875f8e9f10aba4583595c
Velizar <v.angelovtm@gmail.com>
2022-09-26 20:24:01 +0300

    Added edit and delete for different products.

---
+++ SkyLimit/src/main/java/com/unwe/thesis/skylimit/model/binding/ProductUpdateBindingModel.java
@@ -0,0 +1,89 @@
+package com.unwe.thesis.skylimit.model.binding;
+
+import com.unwe.thesis.skylimit.model.entity.enums.CategoryEnum;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+
+public class ProductUpdateBindingModel {
+    private Long id;
+    @NotBlank
+    private String name;
+    @NotNull
+    private BigDecimal price;
+    @NotBlank
+    private String description;
+    @NotBlank
+    private String location;
+    @NotNull
+    private Boolean available;
+    @NotNull
+    private CategoryEnum category;
+    private String imageUrl;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Boolean getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Boolean available) {
+        this.available = available;
+    }
+
+    public CategoryEnum getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryEnum category) {
+        this.category = category;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
